93c5571674b96b05e9d2beb28c67ae509c21b649
Raileanu Nicolaie Alexandru <alecz24@gmail.com>
2018-11-15 16:00:35 +0200

    Update configuration.php

--- configuration.php
+++ configuration.php
@@ -3,11 +3,11 @@
 // DISABLE CACHING ON THE ENTITY IN PREPROCESS HOOKS
 $variables['#cache']['max-age'] = 0;
 
-// SET CACHE CONTEXTS
-    $variables['#cache'] = [
-      'contexts' => [
-        'cookies:CookieWithPriority',
-        'cookies:CookieWithoutPriority',
-      ]
-    ];
+// SET CACHE CONTEXTS FOR CACHE
+$variables['#cache'] = [
+  'contexts' => [
+    'cookies:CookieWithPriority',
+    'cookies:CookieWithoutPriority',
+  ]
+];
     
